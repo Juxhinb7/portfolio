01f9f15c2eabe8f649e2bed09fe86c6db14fcccc
juxhinb7 <juxhinbazelli@gmail.com>
2024-08-12 10:26:46 +0200

    fixed toggle button

--- app/components/theme-toggle.tsx
+++ app/components/theme-toggle.tsx
@@ -5,18 +5,22 @@ import { useEffect, useMemo, useState } from "react";
 import { useTheme } from "next-themes";
 
 const ThemeToggle = () => {
-    const [isMounted, setIsMounted] = useState(false);
     const {theme, setTheme} = useTheme();
+    const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
         let dataTheme = document.documentElement.getAttribute("data-theme") as string;
         setTheme(dataTheme);
+        setMounted(true);
 
     }, []);
+
+    if (!mounted) return null;
     
     return (
         <button aria-label="Toggle light / dark mode" aria-labelledby="label" className="hover:text-gray-400 fixed bottom-5 right-5 z-10" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-            {theme === "light" ? <MdDarkMode className="font-medium text-3xl" /> : <MdLightMode className="font-medium text-3xl" />}
+            {theme === "light" && <MdDarkMode className="font-medium text-3xl" />}
+            {theme === "dark" && <MdLightMode className="font-medium text-3xl" />}
         </button>
 
     )
